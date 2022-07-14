2c7f9c00bfb888613bf97c9656fd1d7837514cd9
Aniket Rai <prosniperaniket@gmail.com>
2022-07-14 19:02:19 +0530

    Update duplicate.java

--- Arrays/duplicate.java
+++ Arrays/duplicate.java
@@ -3,29 +3,43 @@ package Arrays;
 import java.util.Arrays;
 
 public class duplicate {
-    public static void main(String[] args) {
-        // int[] arr = { 1, 3, 4, 2, 2 };
-        int[] arr = { 3, 1, 3, 4, 2 };
-        // Arrays.sort(arr);
-        // int temp = arr[0];
-        // for (int i = 1; i < arr.length; i++) {
-        // if (temp == arr[i]) {
-        // break;
-        // }
-        // temp = arr[i];
-        // }
-        // System.out.println(temp);
-
-        // Method 2
-        int temp = arr[0];
-        for (int i = 0; i < arr.length; i++) {
-            temp = arr[i];
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] == temp) {
-                    break;
+    public static int findDuplicate(int[] nums) {
+        int temp = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            temp = nums[i];
+            // System.out.println(temp);
+            for (int j = 0; j < nums.length; j++) {
+                if (j != i && nums[j] == temp) {
+                    return temp;
                 }
             }
         }
-        System.out.println(temp);
+        return temp;
+
+    }
+
+    public static int findDup1(int[] arr) {
+        Arrays.sort(arr);
+        int temp = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (temp == arr[i]) {
+                break;
+            }
+            temp = arr[i];
+        }
+        return temp;
+    }
+
+    public static int findDup2(int[] arr) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        // int[] arr = { 1, 3, 4, 2, 2 };
+        int[] arr = { 3, 1, 3, 4, 2 };
+
+        // System.out.println(findDuplicate(arr));
+        // System.out.println(findDup1(arr));
+        System.out.println(findDup2(arr));
     }
 }
